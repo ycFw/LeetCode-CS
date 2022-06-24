26ce053729e54765b692c0f5006c32d129e61901
2698676480@qq.com <congyin@microsoft.com>
2022-06-24 12:03:19 +0800

    init

--- LeetCode/0001-0050/001-TwoSum.cs
+++ LeetCode/0001-0050/001-TwoSum.cs
@@ -12,7 +12,6 @@ namespace LeetCode
     {
         public int[] TwoSum(int[] nums, int target)
         {
-            // continue test git reabse
             var dic = new Dictionary<int, int>();
 
             for (int i = 0; i < nums.Length; i++)
@@ -24,7 +23,6 @@ namespace LeetCode
             }
 
             return new int[] { };
-            // test git rebase
         }
     }
 }
